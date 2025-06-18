6b93e3d8963ea1a4e39221763a2212f41a08c3f0
Bariya Vishwas <bariyavasu46@gmail.com>
2025-06-18 20:46:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+ const inputBox = document.getElementById("evaluatedText");
+    const countDisplay = document.getElementById("letterCount");
+
+    inputBox.addEventListener("input", () => {
+      const length = inputBox.value.length;
+      countDisplay.textContent = length;
+    });
